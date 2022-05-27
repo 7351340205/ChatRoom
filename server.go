b239e74ebf446a6080ccaaf075a374646aa0291c
Mazhe <mazhe@wasu.com>
2022-05-27 14:10:40 +0800

    v0.3 用户消息广播

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -12,13 +13,13 @@ type Server struct {
 
 	//在线用户 map+读写锁
 	OnlineMap map[string]*User
-	maplock   sync.RWMutex
+	mapLock   sync.RWMutex
 
 	//消息广播的channel
 	Message chan string
 }
 
-//创建Server
+// NewServer 创建Server
 func NewServer(ip string, port int) *Server {
 	server := &Server{
 		Ip:        ip,
@@ -40,15 +41,15 @@ func (this *Server) ListenMessager() {
 		msg := <-this.Message
 
 		//发送消息至全体在线user
-		this.maplock.Lock()
+		this.mapLock.Lock()
 		for _, cli := range this.OnlineMap {
 			cli.C <- msg
 		}
-		this.maplock.Unlock()
+		this.mapLock.Unlock()
 	}
 }
 
-//处理消息
+// Handler 处理消息
 func (this *Server) Handler(conn net.Conn) {
 	//当前连接的业务
 	fmt.Println("连接建立成功,尝试接收数据,IP:", conn.RemoteAddr())
@@ -56,19 +57,40 @@ func (this *Server) Handler(conn net.Conn) {
 	user := NewUser(conn)
 
 	//用户上线，将用户加入到map中
-	this.maplock.Lock()
+	this.mapLock.Lock()
 	this.OnlineMap[user.Name] = user
-	this.maplock.Unlock()
+	this.mapLock.Unlock()
 
 	//广播当前用户上线消息
 	this.BroadCast(user, "用户已上线")
+
+	//接收客户端信息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "用户已下线")
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn read err:", err)
+				return
+			}
+
+			msg := string(buf[:n-1])
+
+			this.BroadCast(user, msg)
+
+		}
+	}()
 
 	//阻塞
 	select {}
 
 }
 
-//启动服务器
+// Start 启动服务器
 func (this *Server) Start() {
 	//socket Listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
